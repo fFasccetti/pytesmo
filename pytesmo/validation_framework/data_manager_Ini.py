bd26477e8229e4bfc433dc3d6ba2a2afba405812
Christoph Reimer <christoph.reimer@geo.tuwien.ac.at>
2016-05-20 15:37:43 +0200

    remove warnings for parallel processing.

--- pytesmo/validation_framework/data_manager_Ini.py
+++ pytesmo/validation_framework/data_manager_Ini.py
@@ -182,40 +182,40 @@ class DataManager(object):
         try:
             ref_df = reference['class'].read_ts(*args, **reference['kwargs'])
         except IOError:
-            warnings.warn("IOError while reading reference {:}".format(args))
+            # warnings.warn("IOError while reading reference {:}".format(args))
             return None
         except RuntimeError as e:
             if e.args[0] == "No such file or directory":
-                warnings.warn(
-                    "IOError while reading reference {:}".format(args))
+                # warnings.warn(
+                    # "IOError while reading reference {:}".format(args))
                 return None
             else:
                 raise e
 
         if ref_df is None:
-            warnings.warn("No data for reference {:}".format(args))
+            # warnings.warn("No data for reference {:}".format(args))
             return None
 
         if len(ref_df) == 0:
-            warnings.warn("No data for reference {:}".format(args))
+        #     warnings.warn("No data for reference {:}".format(args))
             return None
 
         if self.data_prep is not None:
             ref_df = self.data_prep.prep_reference(ref_df)
 
         if len(ref_df) == 0:
-            warnings.warn("No data for reference {:}".format(args))
+            #warnings.warn("No data for reference {:}".format(args))
             return None
 
         if isinstance(ref_df, pd.DataFrame) == False:
-            warnings.warn("Data is not a DataFrame {:}".format(args))
+            #warnings.warn("Data is not a DataFrame {:}".format(args))
             return None
 
         if self.period is not None:
             ref_df = ref_df[self.period[0]:self.period[1]]
 
         if len(ref_df) == 0:
-            warnings.warn("No data for reference {:}".format(args))
+            #warnings.warn("No data for reference {:}".format(args))
             return None
 
         else:
@@ -248,41 +248,41 @@ class DataManager(object):
         try:
             other_df = other['class'].read_ts(*args, **other['kwargs'])
         except IOError:
-            warnings.warn(
-                "IOError while reading other dataset {:}".format(args))
+            #warnings.warn(
+            #    "IOError while reading other dataset {:}".format(args))
             return None
         except RuntimeError as e:
             if e.args[0] == "No such file or directory":
-                warnings.warn(
-                    "IOError while reading other dataset {:}".format(args))
+                #warnings.warn(
+                #    "IOError while reading other dataset {:}".format(args))
                 return None
             else:
                 raise e
 
         if other_df is None:
-            warnings.warn("No data for other dataset".format(args))
+            #warnings.warn("No data for other dataset".format(args))
             return None
 
         if len(other_df) == 0:
-            warnings.warn("No data for other dataset".format(args))
+            #warnings.warn("No data for other dataset".format(args))
             return None
 
         if self.data_prep is not None:
             other_df = self.data_prep.prep_other(other_df, other_name)
 
         if len(other_df) == 0:
-            warnings.warn("No data for other dataset {:}".format(args))
+            #warnings.warn("No data for other dataset {:}".format(args))
             return None
 
         if isinstance(other_df, pd.DataFrame) == False:
-            warnings.warn("Data is not a DataFrame {:}".format(args))
+            #warnings.warn("Data is not a DataFrame {:}".format(args))
             return None
 
         if self.period is not None:
             other_df = other_df[self.period[0]:self.period[1]]
 
         if len(other_df) == 0:
-            warnings.warn("No data for other dataset {:}".format(args))
+            #warnings.warn("No data for other dataset {:}".format(args))
             return None
 
         else:
